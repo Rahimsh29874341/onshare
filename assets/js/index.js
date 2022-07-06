b597935bc1c7c2c41859295c26f2b38b95283fba
Rahim Shaikh <twicelyrics958@gmail.com>
2022-07-06 17:46:04 +0530

    cors added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,175 @@
-const dropZone = document.querySelector('.drop-zone')
-const browseBtn = document.querySelector('.browseBtn')
-const fileInput = document.querySelector('#fileInput')
+const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#fileInput");
+const browseBtn = document.querySelector("#browseBtn");
 
-const bgProgress = document.querySelector('.bg-progress')
+const bgProgress = document.querySelector(".bg-progress");
+const progressPercent = document.querySelector("#progressPercent");
+const progressContainer = document.querySelector(".progress-container");
+const progressBar = document.querySelector(".progress-bar");
+const status = document.querySelector(".status");
 
-const host = "https://onshare.herokuapp.com/"
-const uploadUrl = `${host}api/files`
-const uploadEmail = `${host}api/files`
+const sharingContainer = document.querySelector(".sharing-container");
+const copyURLBtn = document.querySelector("#copyURLBtn");
+const fileURL = document.querySelector("#fileURL");
+const emailForm = document.querySelector("#emailForm");
 
-dropZone.addEventListener('dragover',(e)=>{
-    e.preventDefault()
-    if (!dropZone.classList.contains('dragged')){
-        dropZone.classList.add('dragged')
+const toast = document.querySelector(".toast");
+
+const baseURL = "https://onshare.herokuapp.com";
+const uploadURL = `${baseURL}/api/files`;
+const emailURL = `${baseURL}/api/files/send`;
+
+const maxAllowedSize = 500 * 1024 * 1024; //500mb
+
+
+browseBtn.addEventListener("click", () => {
+  fileInput.click();
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  //   console.log("dropped", e.dataTransfer.files[0].name);
+  const files = e.dataTransfer.files;
+  if (files.length === 1) {
+    if (files[0].size < maxAllowedSize) {
+      fileInput.files = files;
+      uploadFile();
+    } else {
+      showToast("Max file size is 100MB");
     }
-})
+  } else if (files.length > 1) {
+    showToast("You can't upload multiple files");
+  }
+  dropZone.classList.remove("dragged");
+});
 
-dropZone.addEventListener('dragleave',()=>{
-    dropZone.classList.remove('dragged')
-})
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZone.classList.add("dragged");
 
-dropZone.addEventListener('drop',(e)=>{
-    dropZone.classList.remove('dragged')
-    console.log('drop completed')
-    const files = e.dataTransfer.files
-    if(files.length){
-        fileInput.files = files
-        uploadFile();
+  // console.log("dropping file");
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+  dropZone.classList.remove("dragged");
+
+  console.log("drag ended");
+});
+
+// file input change and uploader
+fileInput.addEventListener("change", () => {
+  if (fileInput.files[0].size > maxAllowedSize) {
+    showToast("Max file size is 100MB");
+    fileInput.value = ""; // reset the input
+    return;
+  }
+  uploadFile();
+});
+
+// sharing container listenrs
+copyURLBtn.addEventListener("click", () => {
+  fileURL.select();
+  document.execCommand("copy");
+  showToast("Copied to clipboard");
+});
+
+fileURL.addEventListener("click", () => {
+  fileURL.select();
+});
+
+const uploadFile = () => {
+  console.log("file added uploading");
+
+  files = fileInput.files;
+  const formData = new FormData();
+  formData.append("myfile", files[0]);
+
+  //show the uploader
+  progressContainer.style.display = "block";
+
+  // upload file
+  const xhr = new XMLHttpRequest();
+
+  // listen for upload progress
+  xhr.upload.onprogress = function (event) {
+    // find the percentage of uploaded
+    let percent = Math.round((100 * event.loaded) / event.total);
+    progressPercent.innerText = percent;
+    const scaleX = `scaleX(${percent / 100})`;
+    bgProgress.style.transform = scaleX;
+    progressBar.style.transform = scaleX;
+  };
+
+  // handle error
+  xhr.upload.onerror = function () {
+    showToast(`Error in upload: ${xhr.status}.`);
+    fileInput.value = ""; // reset the input
+  };
+
+  // listen for response which will give the link
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      onFileUploadSuccess(xhr.responseText);
     }
-})
+  };
 
-fileInput.addEventListener('change',()=>{
-    uploadFile()
-})
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
 
-browseBtn.addEventListener('click',()=>{
-    fileInput.click()
-})
+const onFileUploadSuccess = (res) => {
+  fileInput.value = ""; // reset the input
+  status.innerText = "Uploaded";
 
-const uploadFile = ()=>{
-    const file = fileInput.files[0]
-    const formData = new FormData();
-    formData.append('myfile',file)
+  // remove the disabled attribute from form btn & make text send
+  emailForm[2].removeAttribute("disabled");
+  emailForm[2].innerText = "Send";
+  progressContainer.style.display = "none"; // hide the box
 
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = ()=>{
-        if(xhr.readyState === XMLHttpRequest){
-            console.log(xhr.response);
-        }
-    }
+  const { file: url } = JSON.parse(res);
+  console.log(url);
+  sharingContainer.style.display = "block";
+  fileURL.value = url;
+};
 
-    xhr.upload.onprogress = updateProgress;
-    
-    xhr.open('POST', uploadUrl)
-    xhr.send(formData)
-}
+emailForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // stop submission
 
-const updateProgress = (e)=>{
-    console.log(e)
-    const percent = Math.round(e.loaded / e.total *100)
-    bgProgress.style.width = `${percent}%`
-}
+  // disable the button
+  emailForm[2].setAttribute("disabled", "true");
+  emailForm[2].innerText = "Sending";
+
+  const url = fileURL.value;
+
+  const formData = {
+    uuid: url.split("/").splice(-1, 1)[0],
+    emailTo: emailForm.elements["to-email"].value,
+    emailFrom: emailForm.elements["from-email"].value,
+  };
+  console.log(formData);
+  fetch(emailURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
+        showToast("Email Sent");
+        sharingContainer.style.display = "none"; // hide the box
+      }
+    });
+});
+
+let toastTimer;
+// the toast function
+const showToast = (msg) => {
+  clearTimeout(toastTimer);
+  toast.innerText = msg;
+  toast.classList.add("show");
+  toastTimer = setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+};
